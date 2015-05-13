a8c589c58924dd54896b5a1e90c94d9c57a72f6e
Desmond Kamas <desitootoo+github@gmail.com>
2015-05-12 21:48:23 -0700

    Desmond Commit 2

--- Assemble/src/AbstractCharacter.java
+++ Assemble/src/AbstractCharacter.java
@@ -1,4 +1,61 @@
+import java.awt.Shape;
+import java.util.ArrayList;
 
-public class AbstractCharacter implements Character extends MovingImage {
+
+public abstract class AbstractCharacter extends MovingImage implements Character {
+
+	private double health;
+	private final double MAX_HEALTH;
+	private double power;
+	private double xvel, yvel;
+	
+	public AbstractCharacter(String filename, int x, int y, int w, int h, double maxHealth, double power) {
+		super(filename, x, y, w, h);
+		health = maxHealth;
+		MAX_HEALTH = maxHealth;
+		this.power = power;
+		xvel = 0;
+		yvel = 0;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void jump() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walk(int amt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attack(Enemy other) {
+		other.addHealth(power);
+	}
+
+	@Override
+	public boolean addHealth(double amt) {
+		health -= amt;
+		return health <= 0;
+	}
+
+	public double healthLeft() {
+		return health;
+	}
+
+	@Override
+	public double totalHealth() {
+		// TODO Auto-generated method stub
+		return MAX_HEALTH;
+	}
+
+	@Override
+	public void act(ArrayList<Shape> obstacles) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Assemble/src/Character.java
+++ Assemble/src/Character.java
@@ -1,12 +1,48 @@
+import java.awt.Shape;
+import java.util.ArrayList;
 
 public interface Character {
-
-	
-	public int getX();
-	
-	public int getY();
-	
-	public void act();
-	
-	public 
+	/**
+	 * Moves the Character upwards
+	 */
+	void jump();
+	/**
+	 * Moves the Character horizontally amt distance
+	 * @param amt the distance moved
+	 */
+	void walk(int amt);
+	/**
+	 * Attacks other, causing other to lose health
+	 * @param other the enemy attacked
+	 */
+	void attack(Enemy other);
+	/**
+	 * Performs this Character's special attack.
+	 * If the character does not have one, nothing
+	 * will occur.
+	 */
+	void special();
+	/**
+	 * Adds amt health points to the Character's health.
+	 * @param power the health points added
+	 * @return true if the character is dead, false
+	 * otherwise.
+	 */
+	boolean addHealth(double power);
+	/**
+	 * Returns the remaining health the Character has
+	 * @return the health left
+	 */
+	double healthLeft();
+	/**
+	 * Returns the maximum amount of health the 
+	 * character can have.
+	 * @return the max health
+	 */
+	double totalHealth();
+	/**
+	 * Makes this character act
+	 * @param obstacles Shapes in the way of the Character
+	 */
+	void act(ArrayList<Shape> obstacles);
 }
