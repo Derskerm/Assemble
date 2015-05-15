83aa961f9fd51db5339460737c2dd1ee7173003a
dkamas273 <dkamas273@PR28.hhs.win.fuhsd.org>
2015-05-15 14:03:06 -0700

    Merged

--- Assemble/src/Level/LevelLibrary.java
+++ Assemble/src/Level/LevelLibrary.java
@@ -1,11 +1,35 @@
 package Level;
 
+import java.util.ArrayList;
+
 public class LevelLibrary {
 
-	private LevelLibrary() {
-		
+	private ArrayList<Level> levels;
+	private ArrayList<Boolean> progress;
+	public static final int HIGHEST_LEVEL_NUM = 2;
+	Level currentLevel;
+	
+	public LevelLibrary(int start) {
+		levels = new ArrayList<Level>();
+		for (int i = start; i < HIGHEST_LEVEL_NUM; i++) {
+			levels.add(LevelLibrary.getLevel(i));
+		}
 	}
 	
-	public static 
+	public static Level getLevel(int num) {
+		if (num == 1) {
+			return new LevelOne();
+		} else if (num == 2) {
+			return new LevelTwo();
+		}
+	}
+	
+	public Level getCurrentLevel() {
+		return currentLevel;
+	}
+	
+	public void checkLevels() {
+		
+	}
 	
 }
